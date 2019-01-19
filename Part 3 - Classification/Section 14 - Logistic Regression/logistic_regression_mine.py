a8d30cdca20717a8267495010d6642fe2e867f4c
Kali Prasad <kaliprasad409@gmail.com>
2019-01-19 23:46:21 +0530

    108 video need to complete from satrting

--- Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression_mine.py
+++ Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression_mine.py
@@ -27,7 +27,4 @@ classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-
-#Making confusing matrix
-from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
